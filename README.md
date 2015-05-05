b93b9122502938fef4f0f6981b1ebd118f602dc9
sighmon <simon.loffler@gmail.com>
2015-05-06 00:46:00 +0930

    CryptoAdl.org version 1.0

---
+++ README.md
@@ -0,0 +1,26 @@
+CryptoParty Adelaide
+====================
+
+A party to celebrate cryptography and help you keep your privacy on-line.
+
+[cryptoadl.org](https://cryptoadl.org)
+
+Made with Middleman
+-------------------
+
+To build your own, clone this repository, spool up a server with:
+
+<code>$ middleman server</code>
+
+Once you're happy with the edits, build the site:
+
+<code>$ middleman build</code>
+
+Then deploy however suits you best. I've made a repository of the /build/ folder and pull that onto the server.
+
+---
+
+* Twitter: [@cryptoadl](https://twitter.com/cryptoadl)
+* Facebook: [fb.com/cryptoadl](https://fb.com/cryptoadl)
+* Instagram: [instagram.com/cryptoadl](https://instagram.com/cryptoadl)
+* A loving sibling of [CryptoParty.org](https://cryptoparty.org) (Thanks Asher)
---
+++ source/js/main.js
@@ -0,0 +1,48 @@
+// JS
+
+$( document ).ready(function() {
+
+		var i = 1;
+		
+		function scrambleText(htmlElement, atIndex, original, numberOfLoops, delay) {
+
+			// Flip the letters for the CryptoParty title
+			function randomLetters() {
+				var text = "";
+				var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+				for ( var i=0; i < original.length; i++ ) {
+					text += possible.charAt(Math.floor(Math.random() * possible.length));
+				}
+				return text;
+			};
+
+			setTimeout(function () {
+
+				$(htmlElement)[atIndex].innerHTML = randomLetters();
+				i++;
+				// Number of times to loop
+				if (i < numberOfLoops) {
+					scrambleText(htmlElement, atIndex, original, numberOfLoops, delay);
+				} else {
+					$(htmlElement)[atIndex].innerHTML = original;
+				}
+			}, delay) // Delay between loops
+
+		};
+
+		// Helper function to save the original element
+		function scramble(htmlElement, numberOfLoops, delay) {
+			var allElements = $(htmlElement);
+			for (var i = 0; i < allElements.length; i++) {
+				if (i > 0) {
+					numberOfLoops += 80;
+				};
+				scrambleText(htmlElement, i, allElements[i].innerHTML, numberOfLoops, delay);
+			};
+		}
+
+		// Text to scramble
+		scramble('h1', 100, 50);
+		scramble('h2', 180, 50);
+		scramble('h3', 260, 50);
+});
